db64e0ca578f69912593c4ed6c842fcd64e263d2
opilane <opilane@HP-CZC0359NN8>
2025-04-07 16:07:18 +0300

    Veel on tarvis lisada Reviews

--- GameLib/Views/GameLibDetailsPage.xaml.cs
+++ GameLib/Views/GameLibDetailsPage.xaml.cs
@@ -1,9 +1,18 @@
+using Models;
+
 namespace Views;
 
 public partial class GameLibDetailsPage : ContentPage
 {
-	public GameLibDetailsPage()
+	public GameLibDetailsPage(Games game)
 	{
 		InitializeComponent();
+
+		this.BindingContext = game;
+	}
+
+	async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+	{
+		await Navigation.PopAsync();
 	}
 }
