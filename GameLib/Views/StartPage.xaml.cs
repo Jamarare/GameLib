505ce09d836efa02abe3b4eec809bea90934882c
Wa1ssy <132674823+Wa1ssy@users.noreply.github.com>
2025-04-14 13:51:50 +0300

    Images added code little bit done, colors adjusted

--- GameLib/Views/StartPage.xaml.cs
+++ GameLib/Views/StartPage.xaml.cs
@@ -1,9 +1,39 @@
-namespace Views;
+using Views;
+
+namespace MauiPlanets.Views;
 
 public partial class StartPage : ContentPage
 {
-	public StartPage()
-	{
-		InitializeComponent();
-	}
+    public StartPage()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        if (this.AnimationIsRunning("TransitionAnimation"))
+        {
+            return;
+        }
+
+        var parentAnimation = new Animation();
+
+        //PlanetsAnimation
+        parentAnimation.Add(0.2, 0.4, new Animation(v => imgLogo.Opacity = v, 0, 1, Easing.CubicIn));
+        parentAnimation.Add(0.3, 0.5, new Animation(v => imgSteam.Opacity = v, 0, 1, Easing.CubicIn));
+        parentAnimation.Add(0.4, 0.6, new Animation(v => imgUbisoft.Opacity = v, 0, 1, Easing.CubicIn));
+        parentAnimation.Add(0.5, 0.7, new Animation(v => imgValve.Opacity = v, 0, 1, Easing.CubicIn));
+        parentAnimation.Add(0.6, 0.8, new Animation(v => imgRockstar.Opacity = v, 0, 1, Easing.CubicIn));
+        parentAnimation.Add(0.7, 0.9, new Animation(v => imgXbox.Opacity = v, 0, 1, Easing.CubicIn));
+
+        //intro box
+        parentAnimation.Add(0.7, 1, new Animation(v => imgIntro.Opacity = v, 0, 1, Easing.CubicIn));
+
+        parentAnimation.Commit(this, "TransitionAnimation", 16, 3000, null, null);
+    }
+
+    async void ExploreNow_Clicked(System.Object sender, System.EventArgs e)
+        => Application.Current.MainPage = new NavigationPage(new GameLibPage());
 }
