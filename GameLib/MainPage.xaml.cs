12124d6a3d503afd828b26dc1c810065114e5628
Opilane TTHK <opilane@tthk.ee>
2025-04-28 14:16:55 +0300

    Added navigatsion for categories

--- GameLib/MainPage.xaml.cs
+++ GameLib/MainPage.xaml.cs
@@ -1,25 +1,53 @@
-﻿namespace GameLib
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using Models;
+using Services;
+
+namespace GameLib
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, IQueryAttributable
     {
-        int count = 0;
+
+        public ObservableCollection<Games> FilteredGames { get; set; } = new ObservableCollection<Games>();
 
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
         {
-            count++;
+            if (query.ContainsKey("selectedCategory"))
+            {
+                string selectedCategory = query["selectedCategory"]?.ToString();
+                if (!string.IsNullOrEmpty(selectedCategory))
+                {
+                    LoadGamesByCategory(selectedCategory);  
+                }
+                else
+                {
+                    Debug.WriteLine("Erorr: Choosed category is wrong.");
+                }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            }
         }
+
+        private void LoadGamesByCategory(string category)
+        {
+            var allGames = GameLibService.GetAllGames();
+            var filtered = allGames.Where(g => g.Category.Equals(category, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            FilteredGames.Clear();
+            foreach (var game in filtered)
+            {
+                FilteredGames.Add(game);
+            }
+        }
+
+
+
     }
 
 }
--- GameLib/Models/CategoriesViewModel.cs
+++ GameLib/Models/CategoriesViewModel.cs
@@ -20,17 +20,26 @@ namespace Models
                 new Category { Name = "FPS Games", Icon = "fps_games.png" },
                 new Category { Name = "RPG Games", Icon = "fps_games.png"},
                 new Category { Name = "MOBA Games", Icon = "fps_games.png"},
-                new Category { Name = "Sports Game", Icon = "fps_games.png"},
+                new Category { Name = "Sports Games", Icon = "fps_games.png"},
                 new Category { Name = "Action Games", Icon = "fps_games.png"},
                 new Category { Name = "Free To Play Games", Icon = "fps_games.png"},
-                new Category { Name = "Payed Games", Icon = "fps_games.png"}
+                new Category { Name = "Payed Games", Icon = "fps_games.png"},
+                new Category { Name = "Horror Games", Icon = "fps_games.png"}
             };
-            CategorySelectedCommand = new Command<Category>(OnCategorySelected);
+            CategorySelectedCommand = new Command<Category>( async (selectedCategory) => await OnCategorySelected(selectedCategory)); 
         }
-        private void OnCategorySelected(Category selectedCategory)
+        private async Task OnCategorySelected(Category selectedCategory)
         {
-            
+            if (selectedCategory == null)
+                return;
+
             Debug.WriteLine($"You Choosed Category: {selectedCategory.Name}");
+
+
+            await Shell.Current.GoToAsync($"//MainPage?selectedCategory={selectedCategory.Name}");
+
+
+
         }
     }
 }
