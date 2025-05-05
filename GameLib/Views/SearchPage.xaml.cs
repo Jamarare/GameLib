b9c889c2f3dfe04207d4ef292e9dcb87c357e8f2
Opilane TTHK <markovassiljev01@gmail.com>
2025-05-05 13:22:57 +0300

    Lisatud SearchPage

--- GameLib/Views/SearchPage.xaml.cs
+++ GameLib/Views/SearchPage.xaml.cs
@@ -1,9 +1,52 @@
-namespace Views;
+using Models;
+using Services;
 
-public partial class SearchPage : ContentPage
+namespace Views
 {
-	public SearchPage()
-	{
-		InitializeComponent();
-	}
+    public partial class SearchPage : ContentPage
+    {
+        private List<Games> allGames;
+
+        public SearchPage()
+        {
+            InitializeComponent();
+            LoadGames();
+        }
+
+        private void LoadGames()
+        {
+            allGames = GameLibService.GetAllGames();
+            GamesListView.ItemsSource = allGames;
+        }
+
+        private void OnSearchTextChanged(object sender, TextChangedEventArgs e)
+        {
+            var filtered = allGames
+                .Where(g => g.Name.Contains(e.NewTextValue, StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            GamesListView.ItemsSource = filtered;
+        }
+
+        private void OnGameSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is Games selectedGame)
+            {
+                GameName.Text = selectedGame.Name;
+                GameCategory.Text = selectedGame.Category;
+                GameDescription.Text = selectedGame.Description;
+
+                if (!string.IsNullOrWhiteSpace(selectedGame.HeroImage))
+                {
+                    GameImage.Source = ImageSource.FromFile(selectedGame.HeroImage);
+                }
+                else
+                {
+                    GameImage.Source = null;
+                }
+
+                GameDetailsLayout.IsVisible = true;
+            }
+        }
+    }
 }
