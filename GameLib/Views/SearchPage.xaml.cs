c707acf13263bda21776749941bd64e9c1e34eff
opilane <opilane@HP-CZC0359NN5>
2025-05-05 13:40:00 +0300

    .

--- GameLib/Views/SearchPage.xaml.cs
+++ GameLib/Views/SearchPage.xaml.cs
@@ -37,5 +37,17 @@ namespace Views
                 ((CollectionView)sender).SelectedItem = null;
             }
         }
+        async void BackButton_Clicked(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+        private async void OnReviewsTapped(object sender, EventArgs e)
+        {
+            var game = (sender as Label)?.BindingContext as Games;
+            if (game != null && !string.IsNullOrEmpty(game.ReviewsUrl))
+            {
+                await Launcher.OpenAsync(new Uri(game.ReviewsUrl));
+            }
+        }
     }
 }
